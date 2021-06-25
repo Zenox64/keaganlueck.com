f9955c165fe601e667c19a70e680ca2476e3a9ea
klueck21 <70820765+klueck21@users.noreply.github.com>
2021-06-25 15:25:06 -0500

    responsively working image under name

--- coffee.js
+++ coffee.js
@@ -1,3 +1,40 @@
 function caribou() {
     document.getElementById('outletLogo').src = "caribou.png";
+    //document.getElementById("caribou").style.color = "blue";
+}
+function starbucks() {
+    document.getElementById('outletLogo').src = "caribou.png";
+    //document.getElementById("starbucks").style.color = "blue";
+}
+function dunkinDonuts() {
+    document.getElementById('outletLogo').src = "caribou.png";
+    //document.getElementById("dunkinDonuts").style.color = "blue";
+}
+function timHortons() {
+    document.getElementById('outletLogo').src = "caribou.png";
+    //document.getElementById("timHortons").style.color = "blue";
+}
+function dutchBros() {
+    document.getElementById('outletLogo').src = "caribou.png";
+    //document.getElementById("dutchBros").style.color = "blue";
+}
+function peetsCoffee() {
+    document.getElementById('outletLogo').src = "caribou.png";
+    //document.getElementById("peetsCoffee").style.color = "blue";
+
+}
+function theCoffeeBean() {
+    document.getElementById('outletLogo').src = "caribou.png";
+    //document.getElementById("theCoffeeBean").style.color = "blue";
+
+}
+function biggbyCoffee() {
+    document.getElementById('outletLogo').src = "caribou.png";
+    //document.getElementById("biggbyCoffee").style.color = "blue";
+
+}
+function mcdonalds() {
+    document.getElementById('outletLogo').src = "caribou.png";
+    //document.getElementById("mcdonalds").style.color = "blue";
+
 }
