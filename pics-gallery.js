7d43b63a5be2406dae932a4acba33e4eb33675ba
Keagan Lueck <keaganlueck@gmail.com>
2021-12-11 19:09:44 -0600

    changes made

--- pics-gallery.js
+++ pics-gallery.js
@@ -1,3 +1,17 @@
+var output = document.getElementById("output");
+var input = document.getElementById("inputFar");
+input.addEventListener("input", convert);
+function convert() {
+  var input = document.getElementById("inputFar").value;
+  var outputCels = ((input - 32) * 5) / 9;
+  if (isNaN(outputCels) === true) {
+    output.textContent = "Please enter a number";
+  } else {
+    var outputCelsRound = Math.round(10 * outputCels) / 10;
+    output.textContent = outputCelsRound + "\u00B0" + " C";
+  }
+}
+
 /*var text = document.getElementById("input").value;
 function changingText() {
   text.value = answer;
