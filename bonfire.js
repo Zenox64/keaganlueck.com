dd9f5ed7e06cdab7be8d7f51031e76b06e19a473
unknown <keaganlueck@gmail.com>
2023-05-23 13:42:50 -0500

    phone number added

--- bonfire.js
+++ bonfire.js
@@ -1,10 +1,19 @@
 btnTxt = document.getElementById("btnTxt");
 addrBtn = document.getElementById("addrBtn");
+phnTxt = document.getElementById("phone")
 btnTxt.textContent = "Address: 20713 Ranch Ave, Morgan, MN 56266";
+phnTxt.textContent = "507-430-0823"
 
 function copyText() {
   btnTxt.fontWeight = "normal";
   btnTxt.fontStyle = "italic";
   btnTxt.textContent = "COPIED";
   navigator.clipboard.writeText("20713 Ranch Ave, Morgan, MN 56266");
+}
+
+function copyPhone() {
+  btnTxt.fontWeight = "normal";
+  btnTxt.fontStyle = "italic";
+  btnTxt.textContent = "COPIED";
+  navigator.clipboard.writeText("5074300823");
 }
