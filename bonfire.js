7b95d9633280c538d28f6091e80773112212a766
unknown <keaganlueck@gmail.com>
2023-05-23 13:38:08 -0500

    bonfire page done

--- bonfire.js
+++ bonfire.js
@@ -1,9 +1,10 @@
-function copy() {
-  popup.classList.toggle("show");
-  triangle.classList.toggle("show");
+btnTxt = document.getElementById("btnTxt");
+addrBtn = document.getElementById("addrBtn");
+btnTxt.textContent = "Address: 20713 Ranch Ave, Morgan, MN 56266";
+
+function copyText() {
+  btnTxt.fontWeight = "normal";
+  btnTxt.fontStyle = "italic";
+  btnTxt.textContent = "COPIED";
   navigator.clipboard.writeText("20713 Ranch Ave, Morgan, MN 56266");
-  setTimeout(() => {
-    popup.classList.remove("show");
-    triangle.classList.remove("show");
-  }, 3500);
 }
