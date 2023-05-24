5e04b3e38cf45b82bc2e14b27b27f6296c774a73
Keagan Lueck <keaganlueck@gmail.com>
2023-05-24 00:39:41 -0500

    little changes

--- bonfire.js
+++ bonfire.js
@@ -1,8 +1,10 @@
 btnTxt = document.getElementById("btnTxt");
 addrBtn = document.getElementById("addrBtn");
 phnTxt = document.getElementById("phone")
+lnkTxt = document.getElementById("link")
 btnTxt.textContent = "Address: 20713 Ranch Ave, Morgan, MN 56266";
 phnTxt.textContent = "507-430-0823"
+lnkTxt.textContent = "Share Link"
 
 function copyText() {
   btnTxt.fontWeight = "normal";
@@ -12,8 +14,17 @@ function copyText() {
 }
 
 function copyPhone() {
-  btnTxt.fontWeight = "normal";
+  phnTxt.fontWeight = "normal";
   btnTxt.fontStyle = "italic";
-  btnTxt.textContent = "COPIED";
+  phnTxt.textContent = "COPIED";
   navigator.clipboard.writeText("5074300823");
 }
+
+function copyLink() {
+  lnkTxt.fontWeight = "normal";
+  lnkTxt.fontStyle = "italic";
+  lnkTxt.textContent = "Link copied";
+  navigator.clipboard.writeText("www.keaganlueck.com/bonfire.html");
+}
+
+alert("Share this link with all of your friends")
